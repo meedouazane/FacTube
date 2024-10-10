84e9bdab9273aa6ec1a30095d3cada36452dbb00
Meed <mohamedouazane@yahoo.com>
2024-10-10 13:45:48 +0100

    use pytubefix

--- Models/Speech2Text.py
+++ Models/Speech2Text.py
@@ -3,8 +3,7 @@
 Speech to text module
 """
 import io
-import subprocess
-import yt_dlp
+from pytubefix import YouTube
 from groq import Groq
 from dotenv import load_dotenv
 
@@ -14,36 +13,18 @@ client = Groq()
 
 def extract_audio_from_youtube(url):
     """
-    Extract audio from YouTube video and load it into an in-memory buffer without saving to disk.
-    :param url: URL of the video
-    :return: A BytesIO buffer containing the audio and the video title
+    Extracting audio from YouTube videos
+    :param url: url of the video
+    :return: the path of audio file
     """
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,
-        'noplaylist': True,
-        'extractaudio': True,
-    }
-
-    # Create an in-memory buffer
+    yt = YouTube(url, use_po_token=True)
+    stream = yt.streams.filter(only_audio=True).first()
     audio_buffer = io.BytesIO()
+    stream.stream_to_buffer(audio_buffer)
+    audio_buffer.seek(0)
+    return audio_buffer, yt.title
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # Extract video info
-        info = ydl.extract_info(url, download=False)
-        title = info.get('title')
-        audio_url = info['url']  # URL for the audio stream
 
-        # Use ffmpeg to pipe the audio stream directly into the buffer
-        ffmpeg_process = subprocess.Popen(
-            ['ffmpeg', '-i', audio_url, '-f', 'mp3', 'pipe:1'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL
-        )
-        # Write audio data to buffer
-        audio_buffer.write(ffmpeg_process.stdout.read())
-        audio_buffer.seek(0)
-    return audio_buffer, title
 
 def speech_to_text(url):
     audio_buffer, title = extract_audio_from_youtube(url)
