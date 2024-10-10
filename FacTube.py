d92e622bdbb3edeaad05e014b76fe94e4b60a0e7
Meed <mohamedouazane@yahoo.com>
2024-10-10 16:00:55 +0100

    editing front end

--- FacTube.py
+++ FacTube.py
@@ -1,16 +1,21 @@
 import streamlit as st
 from Models.llama import llama_check
 
+st.set_page_config(
+    page_title="FactTube",
+    page_icon='img/ft_1.png',
+)
 
 # Streamlit app
-st.title("YouTube Fact Checker")
+st.image('img/ft_1.png', width=500)
+
 
 # User input
-youtube_url = st.text_input("Enter YouTube URL:")
+youtube_url = st.text_input(label="Enter YouTube URL:", placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 if st.button("Check Facts"):
     if youtube_url:
-        # Call the fact-checking function
+    # Call the fact-checking function
         result, description = llama_check(youtube_url)
         st.write("Video Title:", description)
         st.write("Fact-Check Result:", result)
